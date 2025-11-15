b508f915251829445a83bbe05bad4e510db71b58
Ruslan <zherdevruslan14@gmail.com>
2025-11-15 23:51:33 +0300

    v1:fix export

--- api/[...path].js
+++ api/[...path].js
@@ -1,4 +1,4 @@
-export default async function handler(request, response) {
+module.exports = async function handler(request, response) {
     response.setHeader(
         "Access-Control-Allow-Origin",
         "https://feelincamatosee.github.io"
